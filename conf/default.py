efbdd44c9e3f147ccd445eba37bb6969739c1941
Donie <donie@canway.net>
2019-07-08 17:07:45 +0800

    update

--- conf/default.py
+++ conf/default.py
@@ -31,9 +31,13 @@ from django.conf.global_settings import *  # noqa
 # APP_TOKEN = 'b78c492f-f044-4b9d-8723-5db9b7b1773f'
 # BK_PAAS_HOST = 'https://paasee-dev.bk.tencent.com'
 
+# APP_ID = 'donie'
+# APP_TOKEN = '3be5f0ba-3993-4178-9667-50140be71ca9'
+# BK_PAAS_HOST = 'https://paas.cwbk.com'
+
 APP_ID = 'donie'
-APP_TOKEN = '3be5f0ba-3993-4178-9667-50140be71ca9'
-BK_PAAS_HOST = 'https://paas.cwbk.com'
+APP_TOKEN = 'ef86a555-d8b7-4f1f-81f4-40add8b3d1fb'
+BK_PAAS_HOST = 'https://paas-bk.bksaas.com'
 
 # 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 DEFAULT_BK_API_VER = 'v2'
